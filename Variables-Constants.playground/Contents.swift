9697fbc1b53a21f39e1fc272f0a347011d28c7cf
Santosh Guruju <santosh.guruju@macrokiosk.com>
2020-10-14 22:09:37 +0800

    Variables, Literals & Constants

---
+++ Variables-Constants.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground" // Variable
+
+let newStr = "Hello, World" // Constant
+
+// Literals
+// Binary literal
+var binaryLiteral = 0b111111
+print(binaryLiteral)
+
+// Octal literal
+var octalLiteral = 0o1211311
+print(octalLiteral)
+
+// Hexa Literal
+var hexaLiteral = 0x1fp12 - 12
+print(hexaLiteral)
+
+// Decimal Literal
+var decimalLiteral = 42425
+print(decimalLiteral)
+
+var floatliteral = 12.31
+print(floatliteral)
+
+var exponentLiteral = 3.143131e2
+print(exponentLiteral)
+
